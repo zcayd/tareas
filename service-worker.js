a3f45d91e5fa2de028f97d082359c76c10e809e5
ZCAYD <91034707+zcayd@users.noreply.github.com>
2025-07-20 14:23:30 -0500

    update

--- service-worker.js
+++ service-worker.js
@@ -1,70 +1,86 @@
-;
-//asignar un nombre y versión al cache
-const CACHE_NAME = 'DCristo Play',
-  urlsToCache = [
-    './',
-    './index.php',
-    './style.css',
+const CACHE_NAME = 'Tareas-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './index.php',
+  './style.css',
 
+  // JS principales
+  './script.js',
+  './regist_serviceWorker.js',
 
-    './script.js',
+  // Archivos en /js/
+  './js/main.js',
+  './js/render.js',
+  './js/modal.js',
+  './js/storage.js',
+  './js/utils.js',
+  './js/filters.js',
+  './js/theme.js',
 
+  // Imagen
+  './assets/img/to-do.png',
 
-    './regist_serviceWorker.js',
+  // Manifest
+  './manifest.json',
 
-    './img/to-do.png',
-  ]
+  // Iconos si los tienes en ./assets/icons/
+  './assets/icons/copy.svg',
+  './assets/icons/danger.svg',
+  './assets/icons/edit.png',
+  './assets/icons/edit.svg',
+  './assets/icons/file-pen.svg',
+  './assets/icons/info.svg',
+  './assets/icons/list-plus.svg',
+  './assets/icons/list-todo.svg',
+  './assets/icons/moon.svg',
+  './assets/icons/plus.svg',
+  './assets/icons/share.svg',
+  './assets/icons/success.svg',
+  './assets/icons/sun.svg',
+  './assets/icons/trash.svg',
+  './assets/icons/warning.svg',
+  './assets/icons/icon-512x512.png',
+];
 
-//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+// Install
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache)
-          .then(() => self.skipWaiting())
-      })
-      .catch(err => console.log('Falló registro de cache', err))
-  )
-})
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+      .catch(err => console.error('❌ Falló el cache', err))
+  );
+});
 
-//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+// Activate
 self.addEventListener('activate', e => {
-  const cacheWhitelist = [CACHE_NAME]
-
+  const cacheWhitelist = [CACHE_NAME];
   e.waitUntil(
     caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName)
+      .then(names =>
+        Promise.all(
+          names.map(name => {
+            if (!cacheWhitelist.includes(name)) {
+              return caches.delete(name);
             }
           })
         )
-      })
-      // Le indica al SW activar el cache actual
+      )
       .then(() => self.clients.claim())
-  )
-})
+  );
+});
 
-//cuando el navegador recupera una url
+// Fetch
 self.addEventListener('fetch', e => {
-  //Responder ya sea con el objeto en caché o continuar y buscar la url real
   e.respondWith(
-    caches.match(e.request)
-      .then(res => {
-        if (res) {
-          //recuperar del cache
-          return res
-        }
-        //recuperar de la petición a la url
-        return fetch(e.request)
-      })
-  )
-})
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
 
-
+// Sync
 self.addEventListener('sync', event => {
   if (event.tag === 'sync-tasks') {
     event.waitUntil(syncPendingTasks());
@@ -72,8 +88,6 @@ self.addEventListener('sync', event => {
 });
 
 async function syncPendingTasks() {
-  // Aquí podrías hacer fetch a una API, o simular almacenamiento local.
   console.log('🔁 Sincronizando tareas...');
-  // Simulación
   return Promise.resolve();
 }
