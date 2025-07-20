56aa2cc6f67e2e0eb56422f2b95f301430990818
ZCAYD <91034707+zcayd@users.noreply.github.com>
2025-07-20 16:07:59 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,12 @@ const CACHE_NAME = 'Tareas-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './index.php',
   './style.css',
+  './manifest.json',
 
-  // JS principales
+  // Scripts
   './script.js',
   './regist_serviceWorker.js',
-
-  // Archivos en /js/
   './js/main.js',
   './js/render.js',
   './js/modal.js',
@@ -18,13 +16,10 @@ const urlsToCache = [
   './js/filters.js',
   './js/theme.js',
 
-  // Imagen
+  // Imágenes
   './assets/img/to-do.png',
 
-  // Manifest
-  './manifest.json',
-
-  // Iconos si los tienes en ./assets/icons/
+  // Otros iconos
   './assets/icons/copy.svg',
   './assets/icons/danger.svg',
   './assets/icons/edit.png',
@@ -39,55 +34,5 @@ const urlsToCache = [
   './assets/icons/success.svg',
   './assets/icons/sun.svg',
   './assets/icons/trash.svg',
-  './assets/icons/warning.svg',
-  './assets/icons/icon-512x512.png',
+  './assets/icons/warning.svg'
 ];
-
-// Install
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-      .catch(err => console.error('❌ Falló el cache', err))
-  );
-});
-
-// Activate
-self.addEventListener('activate', e => {
-  const cacheWhitelist = [CACHE_NAME];
-  e.waitUntil(
-    caches.keys()
-      .then(names =>
-        Promise.all(
-          names.map(name => {
-            if (!cacheWhitelist.includes(name)) {
-              return caches.delete(name);
-            }
-          })
-        )
-      )
-      .then(() => self.clients.claim())
-  );
-});
-
-// Fetch
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
-});
-
-// Sync
-self.addEventListener('sync', event => {
-  if (event.tag === 'sync-tasks') {
-    event.waitUntil(syncPendingTasks());
-  }
-});
-
-async function syncPendingTasks() {
-  console.log('🔁 Sincronizando tareas...');
-  return Promise.resolve();
-}
